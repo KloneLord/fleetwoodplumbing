1b4e6ec5805a4daa09c8c7b0afeaf7f4fe14f288
KloneLord <mathewnorton87@gmail.com>
2024-12-26 09:07:37 +1100

    Update hash_password.js

--- middleware/hash_password.js
+++ middleware/hash_password.js
@@ -1,1 +1,11 @@
 
+// hash_password.js
+import bcrypt from 'bcrypt';
+
+const hashPassword = async (password) => {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    return hashedPassword;
+};
+
+export default hashPassword;
