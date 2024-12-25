38110aa204fe4e30ec55290398d965948a21789c
KloneLord <mathewnorton87@gmail.com>
2024-12-26 08:54:54 +1100

    Delete middleware directory

--- middleware/error_middleware.js
+++
@@ -1,6 +0,0 @@
-﻿const error_middleware = (err, req, res, next) => {
-    console.error(err.message);
-    res.status(500).json({ message: 'Internal server error' });
-};
-
-export default error_middleware;
