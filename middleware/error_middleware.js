f5e4f723e33c49db03732eab09f6707fd3af1696
KloneLord <mathewnorton87@gmail.com>
2024-12-26 09:07:50 +1100

    Update error_middleware.js

--- middleware/error_middleware.js
+++ middleware/error_middleware.js
@@ -1,1 +1,8 @@
 
+// error_middleware.js
+const errorMiddleware = (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong!' });
+};
+
+export default errorMiddleware;
